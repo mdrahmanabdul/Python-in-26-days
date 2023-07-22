3b9cc36a690f4a281ad4766c3facc0dacbe1f014
rahman <rahman@Mohammeds-MacBook-Air.local>
2023-07-22 12:50:43 +0530

    Fi-Add : Functional Programming done

---
+++ Day16-Modules and Packages/functions.py
@@ -0,0 +1,3 @@
+def print_details (name, age, phone_num, email_address):
+    print('Name : {}\nAge : {}\nphone number : {}\nemail : {}\n'.format(name,age,phone_num,email_address))
+            
---
+++ Day16-Modules and Packages/pkg/my_funcs.py
@@ -0,0 +1,9 @@
+def func1():
+    print('It is a function in pkg directory')
+    print('Keep smiling :)')
+
+def func2():
+    print('Rahman is a good coder')
+    print('Please give a star to his repo in GitHub')
+    print('He works so hard to share his knowledge to peer coders')
+    
