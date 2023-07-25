f52d3748b1b8eb585739cad489d058180648a1a5
rahman <rahman@Mohammeds-MacBook-Air.local>
2023-07-25 10:10:21 +0530

    Fi_Upd Remaining topics are uploaded successfully

--- Day23-File Input:Output/fileIO.py
+++ Day23-File Input:Output/fileIO.py
@@ -63,3 +63,75 @@ data = f.readlines() #To read all the lines in a file and form a list of lines.
 with open('messages.txt','r') as f:
     data = f.read()
     print(data)
+
+#MOVING WITHIN THE FILE
+'''
+- When we are reading a file or writing a file, the next read or write operation is performed from the next
+  character/byte as compared to the previous read/write operation
+- For example : If we read the first character from a file using f.read(1), next call to f.read(1) will 
+  automatically read the second character in the file.
+- To move to the desired location we can use the f.seek method
+- Syntax : f.seek(offset,reference)
+
+#REFERENCE VALUE :
+0 = begening of the file
+1 = Current position
+2 = End of the file
+'''
+# f.seek(512,0) -> moves to position 512 from beginning of the file
+#f.seek(0,2) -> moves to end of the file
+
+#SERIALIZATION AND DESERIALIZATION
+'''
+- Compared to strings, reading/writing numbers from/to a file is tedious
+- write() writes string to the file and read() returns a string read from a file.
+- So, we need to perform conversions while read/write.
+'''
+
+#Program
+f = open('Numbers.txt','w+')
+f.write(str(6300481313)+'\n') #converting the number to a string and then writing to the file
+f.write(str(13.45)) #converting the float to a string and writing to the file.
+f.seek(0) #moving to the beginning of the file
+a = int(f.readline()) #converting to int and storing in 'a'
+b = float(f.readline()) #converting to float and storing in 'a'
+print(a+a)
+print(b+b)
+
+'''
+- If we need to read/write more complicated data in the form of tuple, dictionaries etc from/to a file
+  it becomes even more difficult.
+- We can use .json to do this task.
+- json module converts the Python data into appropriate JSON types before writing data to a file. Likewise
+  it converts JSON types read from a file into Python data. 
+  Converting python data -> JSON types = Serialization
+  Converting JSON types -> Python data = Deserialization
+'''
+#Program : Let's serialize and Deserialize a list
+#same thing we can for dict and tuples also.
+import json
+f = open('Serialization.txt','w+') #opening the file for write and read operations
+lst = [10,20,30,40,50,60,70,80,90,100]
+json.dump(lst,f) 
+f.seek(0)
+inlst = json.load(f)
+print(inlst)
+f.close()
+
+#FILE AND DIRECTORY OPERATIONS
+import os 
+import shutil
+print(f"OS : {os.name}") #prints 'posix'
+print(os.getcwd())
+print(os.listdir('.')) #prints the current directory
+print(os.listdir('..')) #prints the parent of current directory
+
+
+
+
+
+#NOTE : Serialization of User defined type is skipped because it is rarely used.
+
+
+
+
